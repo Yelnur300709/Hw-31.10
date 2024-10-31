c8c11a1af8d702f5d50941e42d327b00abb194c6
Yelnur300709 <mirasleha8@gmail.com>
2024-10-31 18:52:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let slicesPerPizza = 8;
+let numberOfPizzas = 5;
+
+
+let totalSlices = slicesPerPizza * numberOfPizzas;
+
+
+console.log( totalSlices);
+
+
+let totalChocolates = 24; 
+let numberOfFriends = 6;   
+
+
+let chocolatesPerFriend = totalChocolates / numberOfFriends;
+
+
+console.log( chocolatesPerFriend );
+
+
+let plantHeight = 10; 
+
+
+plantHeight += 2; 
+
+
+console.log( plantHeight );
